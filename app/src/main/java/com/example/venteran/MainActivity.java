c87c9bd5bbd73badc318f51c7268351df6c2d583
Mohit Divya <ap150751@gmail.com>
2022-02-16 15:58:10 +0530

    hello git

--- app/src/main/java/com/example/venteran/MainActivity.java
+++ app/src/main/java/com/example/venteran/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Aditya");
         System.out.println("Deku");
+        System.out.println("Aquel");
     }
 }
