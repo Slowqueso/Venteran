dd64c6485b38f0748a55980c8f86f969a147dacd
aditya-user <adiunni74@gmail.com>
2022-02-23 17:35:19 +0530

    added countrycodepicker ,otpauth layout and setprofile layout

---
+++ app/src/main/java/com/example/venteran/otpAuthentication.java
@@ -0,0 +1,14 @@
+package com.example.venteran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class otpAuthentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_authentication);
+    }
+}
---
+++ app/src/main/java/com/example/venteran/setProfile.java
@@ -0,0 +1,14 @@
+package com.example.venteran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_profile);
+    }
+}
