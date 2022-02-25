93f6d4451c86241e67284de4c6c441f2aba803f1
Slowqueso <lyndabarn2309@gmail.com>
2022-02-25 20:26:12 +0530

    Kartik Hot AF

--- app/src/main/java/com/example/venteran/MainActivity.java
+++ app/src/main/java/com/example/venteran/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Aditya");
         System.out.println("Deku");
         System.out.println("Aquel");
+        System.out.println("Kartik");
     }
 }
